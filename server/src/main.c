a616ea1a403e7285115d24add318f13473c89240
Emilio <xxglobalelitexx123@gmail.com>
2026-03-20 10:25:04 +0100

    Removed: Code that is not used anymore in main.c

--- server/src/main.c
+++ server/src/main.c
@@ -17,43 +17,14 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "cli/cli.h"
 #include "process_manager/process_manager.h"
 #include "parent_supervisor.h"
 #include "process_lifecycle.h"
-#include "benchmark/benchmark.h"
-#include "Datatypes/Unordered_List.h"
-
-/**
- * @brief CLI help callback
- */
-static void help_callback(void)
-{
-    printf("Energy Saver Deluxe\n");
-    printf("Usage: EnergySaverDeluxe [options]\n\n");
-    printf("Options:\n");
-    printf("  --port, -p <integer>  Server port (not yet implemented)\n");
-    printf("  --help, -h            Show this help message\n");
-    exit(0);
-}
 
 // TODO Add restart of processes when api error etc, logger macro?
 
 int main(int argc, char **argv)
 {
-    // Parse command line arguments
-    CLI cli = {0};
-    int port_argument_data = 0;
-
-    CLI_Argument_Add(&cli, "--port", "-p", Argument_Option_Integer, &port_argument_data);
-    CLI_Argument_Add_Callback(&cli, "--help", "-h", help_callback);
-
-    if (!CLI_Parse(&cli, argc, argv))
-    {
-        printf("Failed to parse CLI arguments!\n");
-        return -1;
-    }
-
     // Fork to create parent supervisor and process manager child
     ProcessManager process_manager;
     pid_t process_manager_pid = fork();
